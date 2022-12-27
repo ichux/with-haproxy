6ac7594c542008af020411baf8e8c93dfaa0d0d6
Chukwudi Nwachukwu <theichux@gmail.com>
2022-12-27 12:38:32 +0100

    latest code

--- v3/runner.sh
+++ v3/runner.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+docker stop `docker ps -a | awk '{print $(NF)}' | grep hap | sed 's/==.*//g' | tr '\n' ' '` > /dev/null 2>&1;
+docker rm `docker ps -a | awk '{print $(NF)}' | grep hap | sed 's/==.*//g' | tr '\n' ' '` > /dev/null 2>&1;
+
 docker-compose -p hxy up --build -d
 
 # clean up
 docker stop haproxy_cf_base > /dev/null 2>&1 && docker rm haproxy_cf_base > /dev/null 2>&1
 
 # connect from another image
-docker run --tty -d --name hxy-haproxy_if_web3-1 --net hxy_net haproxy_if_base
-docker exec -it hxy-haproxy_if_web3-1 python read_net_global.py 1
+docker run --rm -it --net hxy_net haproxy_if_base python read_net_global.py 1
+
+# docker run --rm -it --net hxy_net haproxy_if_base python read_net_2_global.py 1
+# docker run --rm -it --net hxy_net curlimages/curl:7.86.0 -I http://haproxy_if_web1:12345
+
+docker logs -f haproxy_cf_app

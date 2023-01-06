fa74374a711b1178faceb7529bf42877ba4dc642
Chukwudi Nwachukwu <theichux@gmail.com>
2023-01-06 19:22:59 +0100

    refactored

--- v5/itworks.py
+++ v5/itworks.py
@@ -1,36 +1,35 @@
+import io
+
 import numpy as np
 from matplotlib import pyplot
-
-import io
 
 
 def bufio():
     pyplot.figure()
     pyplot.plot([1, 2])
     pyplot.title("test")
+
     buffer = io.BytesIO()
-    pyplot.savefig(buffer, format='png')
-    pyplot.savefig("real.png", format='png')
+    pyplot.savefig(buffer, format="png")
+    pyplot.savefig("real.png", format="png")
+
     buffer.seek(0)
     data = buffer.read()
     buffer.close()
-    # f = open('copy.png', 'w')
-    # f.write(data)
-    # f.close()
 
-    with open('copy.png', 'wb') as img:
+    with open("copy.png", "wb") as img:
         img.write(data)
 
 
+def save_to():
+    ys = 200 + np.random.randn(100)
+    x = [x for x in range(len(ys))]
+    pyplot.plot(x, ys, "-")
+    pyplot.fill_between(x, ys, 195, where=(ys > 195), facecolor="g", alpha=0.6)
+    pyplot.title("Sample Visualization")
+    # pyplot.show()
+    pyplot.savefig("points.png")
+
+
 bufio()
-
-ys = 200 + np.random.randn(100)
-x = [x for x in range(len(ys))]
-
-pyplot.plot(x, ys, '-')
-pyplot.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
-
-pyplot.title("Sample Visualization")
-# pyplot.show()
-
-pyplot.savefig("points.png")
+save_to()
--- v6/web/app.py
+++ v6/web/app.py
@@ -1,9 +1,11 @@
-from io import BytesIO
 from base64 import encodebytes
+from io import BytesIO
+
 from flask import Flask, make_response, request
 from PIL import Image
 
 app = Flask(__name__)
+
 
 @app.route("/image", methods=["POST"])
 def post_image():
@@ -11,10 +13,10 @@ def post_image():
         image = Image.open(BytesIO(image.read()))
         buffer = BytesIO()
         image.save(buffer, format="PNG")
-        return make_response(encodebytes(buffer.getvalue()).decode('ascii'), 200)
+        return make_response(encodebytes(buffer.getvalue()).decode("ascii"), 200)
     else:
         return make_response("`image` field is required.", 400)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-

a67cc0cd6e33226bc1a8e27b34d849abea818bae
Chukwudi Nwachukwu <theichux@gmail.com>
2023-07-26 05:29:20 +0100

    made some improvements

--- v6/web/README.md
+++ v6/web/README.md
@@ -3,7 +3,7 @@
 ```shell
 docker compose up -d 
 
-curl -sX POST -F image=@path/to/an/image 'http://localhost:5000/image'\
+curl -sX POST -F image=@path/to/an/image 'http://localhost:5000'\
     | base64 -d > image2.png
 # Then display image2.png in your image viewer
 ```
--- v6/web/app.py
+++ v6/web/app.py
@@ -7,12 +7,13 @@ from PIL import Image
 app = Flask(__name__)
 
 
-@app.route("/image", methods=["POST"])
-def post_image():
+@app.route("", methods=["POST"])
+def upload():
     if (image := request.files.get("image")) is not None:
         image = Image.open(BytesIO(image.read()))
         buffer = BytesIO()
         image.save(buffer, format="PNG")
+        
         return make_response(encodebytes(buffer.getvalue()).decode("ascii"), 200)
     else:
         return make_response("`image` field is required.", 400)
